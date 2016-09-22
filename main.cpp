5d63fa06960ef307fabc5d8206df83f332839f95
Ahmed Elnoamany <ahmedgouda9484@gmail.com>
2016-09-22 11:52:47 -0500

    Initialized project. Added simple difficulty prompt in main.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: Ahmed Elnoamany
+ */
+#include <cstdlib>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    bool exit = false;
+    int difficulty;
+    printf("\t\tTIC TAC TOE\n");
+    printf("----------------------------------------\n");
+    while(exit){
+        printf("Choose a difficulty:\n");
+        printf("1.Easy\n");
+        printf("2.Medium\n");
+        printf("3.Hard\n");
+        scanf("%d", %difficulty);
+    }
+    return 0;
+}
+
